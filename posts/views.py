0f74cd87359a5457138e440b5e78627695c6f271
testeandoando <apuntusweb@gmail.com>
2019-04-06 11:47:05 -0300

    9. Introducción al template system

--- posts/views.py
+++ posts/views.py
@@ -1,43 +1,42 @@
 """Posts views"""
 
-from django.http import HttpResponse
+from django.shortcuts import render
+
 from datetime import datetime
 
 
 posts = [
   {
-    'name': 'Mont Blac',
-    'user': 'Yesica Cortes',
+    'title': 'Mont Blanc',
+    'user': {
+      'name': 'Yésica Cortés',
+      'picture': 'https://picsum.photos/60/60/?image=1027'
+    },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-    'picture': 'https://picsum.photos/200/200/?image=1036',
+    'photo': 'https://picsum.photos/800/600?image=1036',
   },
   {
-    'name': 'Mont Blac',
-    'user': 'Yesica Cortes',
+    'title': 'Via Láctea',
+    'user': {
+      'name': 'Christian Van der Henst',
+      'picture': 'https://picsum.photos/60/60/?image=1005'
+    },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-    'picture': 'https://picsum.photos/200/200/?image=1036',
+    'photo': 'https://picsum.photos/800/800/?image=903',
   },
   {
-    'name': 'Mont Blac',
-    'user': 'Yesica Cortes',
+    'title': 'Nuevo auditorio',
+    'user': {
+      'name': 'Uriel (thespianartist)',
+      'picture': 'https://picsum.photos/60/60/?image=883'
+    },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-    'picture': 'https://picsum.photos/200/200/?image=1036',
-  },
+    'photo': 'https://picsum.photos/500/700/?image=1076',
+  }
 ]
 
 
 def list_posts(request):
   """List existing posts"""
-  content = []
-
-  for post in posts:
-    content.append("""
-      <p>
-        <strong>{name}</strong>
-        <small>{user} - <i>{timestamp}</i></small>
-        <figure><img src="{picture}"></figure>
-      </p>
-    """.format(**post))
-    
-  return HttpResponse('<br>'.join(content))
+  return render(request, 'feed.html', { 'posts': posts })
 
