f15f996868a87c10e79be74093a72895cd299686
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2019-04-20 18:50:45 -0300

    22. Signup

--- users/views.py
+++ users/views.py
@@ -7,6 +7,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+# Exceptions
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 def login_view(request):
     """Login view"""
     if request.method == 'POST':
@@ -22,9 +29,33 @@ def login_view(request):
     else:
         return render(request, 'users/login.html')
 
+
 @login_required
 def logout_view(request):
     """Logout a user"""
     logout(request)
     return redirect('login')
 
+
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
