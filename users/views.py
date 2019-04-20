a73a9cd2b04178f985fd429ee715d6ae7521fafe
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2019-04-20 18:09:19 -0300

    21. Logout

--- users/views.py
+++ users/views.py
@@ -3,8 +3,9 @@
 """
 
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     """Login view"""
@@ -21,6 +22,9 @@ def login_view(request):
     else:
         return render(request, 'users/login.html')
 
-
-
+@login_required
+def logout_view(request):
+    """Logout a user"""
+    logout(request)
+    return redirect('login')
 
